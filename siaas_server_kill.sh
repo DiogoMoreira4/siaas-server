9d1c0e7183a43f83f4394717dfcd4b1f59a9cdf6
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-10-24 14:10:43 +0100

    Restrict killing scope of the server kill script

--- siaas_server_kill.sh
+++ siaas_server_kill.sh
@@ -7,4 +7,4 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-ps -ef | grep siaas.*server | grep -v grep | awk {'print $2'} | xargs kill -9 2> /dev/null
+ps -ef | grep "siaas_server" | grep -v grep | awk {'print $2'} | xargs kill -9 2> /dev/null
