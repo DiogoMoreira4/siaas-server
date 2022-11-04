e21c707c0d35b63fc99ff520b1cf3b0476c494dc
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-11-04 02:25:09 +0000

    Default limit output in historical data API

--- siaas_routes.py
+++ siaas_routes.py
@@ -237,7 +237,7 @@ def agents_configs_id(agent_uid):
 @app.route('/siaas-server/agents/history', methods=['GET'], strict_slashes=False)
 def agents_history():
     module = request.args.get('module', default='*', type=str)
-    limit_outputs = request.args.get('limit', default=0, type=int) # 0 equates to having no output limit (as per MongoDB spec)
+    limit_outputs = request.args.get('limit', default=1000, type=int) # 0 equates to having no output limit (as per MongoDB spec)
     days = request.args.get('days', default=15, type=int)
     sort_by = request.args.get('sort', default="date", type=str)
     older_first = request.args.get('older', default=0, type=int)
@@ -268,7 +268,7 @@ def agents_history():
 @app.route('/siaas-server/agents/history/<agent_uid>', methods=['GET'], strict_slashes=False)
 def agents_history_id(agent_uid):
     module = request.args.get('module', default='*', type=str)
-    limit_outputs = request.args.get('limit', default=0, type=int) # 0 equates to having no output limit (as per MongoDB spec)
+    limit_outputs = request.args.get('limit', default=1000, type=int) # 0 equates to having no output limit (as per MongoDB spec)
     days = request.args.get('days', default=15, type=int)
     sort_by = request.args.get('sort', default="date", type=str)
     older_first = request.args.get('older', default=0, type=int)
