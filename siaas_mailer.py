20f8db96608706e905ca24c2b0bd42fb47406ca1
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-12-15 18:58:22 +0000

    Implemented different types of reporting: all, vuln_only, exploit_only

--- siaas_mailer.py
+++ siaas_mailer.py
@@ -19,6 +19,7 @@ receiver_email = "siaas.iscte@gmail.com"
 smtp_pwd = "mdbnifhmquaexxka"
 smtp_server = "smtp.gmail.com"
 smtp_tls_port = "587"
+report_type = "vuln_only" # all, vuln_only, exploit_only
 
 # Generate global variables from the configuration file
 config_dict = siaas_aux.get_config_from_configs_db(convert_to_string=True)
@@ -51,18 +52,80 @@ db_collection = siaas_aux.connect_mongodb_collection(
 
 
 out_dict=siaas_aux.get_dict_current_agent_data(db_collection, agent_uid=None, module="portscanner")
-out_dict_str=pprint.pformat(out_dict)
+new_dict={}
+
+if report_type.lower() == "all":
+    new_dict = out_dict
+else:
+    try:
+        for a in out_dict.keys():
+            for b in out_dict[a].keys():
+                if b == "portscanner":
+                    for c in out_dict[a][b].keys():
+                        for d in out_dict[a][b][c].keys():
+                            if d == "scanned_ports":
+                                for e in out_dict[a][b][c][d].keys():
+                                    for f in out_dict[a][b][c][d][e].keys():
+                                        if f == "scan_results":
+                                            for g in out_dict[a][b][c][d][e][f].keys():
+                                                for h in out_dict[a][b][c][d][e][f][g].keys():
+                                                    if h == "vulners" or h == "vulscan":
+                                                        if report_type.lower() == "vuln_only":
+                                                            if a not in new_dict.keys():
+                                                                 new_dict[a]={}
+                                                            if b not in new_dict[a].keys():
+                                                                 new_dict[a][b]={}
+                                                            if c not in new_dict[a][b].keys():
+                                                                 new_dict[a][b][c]={}
+                                                            if d not in new_dict[a][b][c].keys():
+                                                                 new_dict[a][b][c][d]={}
+                                                            if e not in new_dict[a][b][c][d].keys():
+                                                                 new_dict[a][b][c][d][e]={}
+                                                            if f not in new_dict[a][b][c][d][e].keys():
+                                                                 new_dict[a][b][c][d][e][f]={}
+                                                            if g not in new_dict[a][b][c][d][e][f].keys():
+                                                                new_dict[a][b][c][d][e][f][g]={}
+                                                            new_dict[a][b][c][d][e][f][g][h]=out_dict[a][b][c][d][e][f][g][h]
+                                                        else: # exploit_only (default)
+                                                            for i in out_dict[a][b][c][d][e][f][g][h].keys():
+                                                                for j in out_dict[a][b][c][d][e][f][g][h][i].keys():
+                                                                    if "siaas_exploit_tag" in out_dict[a][b][c][d][e][f][g][h][i][j]:
+                                                                        if a not in new_dict.keys():
+                                                                            new_dict[a]={}
+                                                                        if b not in new_dict[a].keys():
+                                                                            new_dict[a][b]={}
+                                                                        if c not in new_dict[a][b].keys():
+                                                                            new_dict[a][b][c]={}
+                                                                        if d not in new_dict[a][b][c].keys():
+                                                                            new_dict[a][b][c][d]={}
+                                                                        if e not in new_dict[a][b][c][d].keys():
+                                                                            new_dict[a][b][c][d][e]={}
+                                                                        if f not in new_dict[a][b][c][d][e].keys():
+                                                                            new_dict[a][b][c][d][e][f]={}
+                                                                        if g not in new_dict[a][b][c][d][e][f].keys():
+                                                                            new_dict[a][b][c][d][e][f][g]={}
+                                                                        if h not in new_dict[a][b][c][d][e][f][g].keys():
+                                                                            new_dict[a][b][c][d][e][f][g][h]={}
+                                                                        if i not in new_dict[a][b][c][d][e][f][g][h].keys():
+                                                                            new_dict[a][b][c][d][e][f][g][h][i]={}
+                                                                        new_dict[a][b][c][d][e][f][g][h][i][j]=out_dict[a][b][c][d][e][f][g][h][i][j]
+    except Exception as e:
+        print(str(e))
+
+if len(new_dict) == 0:
+    new_dict_str = "No results available."
+else:
+    new_dict_str=pprint.pformat(new_dict, width=999)
+#print(new_dict_str)
 
 file_to_write="./tmp/siaas_report_"+datetime.now().strftime('%Y%m%d%H%M%S')+".csv"
 os.makedirs(os.path.dirname(os.path.join(sys.path[0], file_to_write)), exist_ok=True)
 with open(file_to_write, 'w') as f:
-    #w = csv.DictWriter(f, out_dict.keys())
+    #w = csv.DictWriter(f, new_dict.keys())
     #w.writeheader()
-    #w.writerow(out_dict)
+    #w.writerow(new_dict)
     w = csv.writer(f)
-    w.writerows(out_dict.items())
-
-#print(out_dict_str)
+    w.writerows(new_dict.items())
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "SIAAS Report from "+datetime.utcnow().strftime('%Y-%m-%d at %H:%M')+" "+datetime.now().astimezone().tzname()
@@ -89,7 +152,7 @@ html = """\
   </body>
 </html>
 """
-output = out_dict_str
+output = new_dict_str
 
 # Turn these into plain/html MIMEText objects
 part1 = MIMEText(text, "plain")
@@ -108,7 +171,9 @@ with open(file_to_write, "r") as file:
   Name=os.path.basename(file_to_write)
   )
 part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(file_to_write)
-message.attach(part)
+
+if len(new_dict) > 0:
+    message.attach(part)
 
 # Create secure connection with server and send email
 context = ssl.create_default_context()
