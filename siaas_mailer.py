109e5027c267c5df2b4cdd5eb0f88ac732c26f87
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-12-31 15:48:06 +0000

    Autopep

--- siaas_mailer.py
+++ siaas_mailer.py
@@ -70,15 +70,16 @@ def send_siaas_email(db_collection, smtp_account, smtp_pwd, smtp_receivers, smtp
                     csv_contents.append([a, c, d, new_dict[a][b][c][d]])
 
     try:
-       locale.setlocale(locale.LC_ALL, '')
-       dec_pt_chr=locale.localeconv()['decimal_point']
-       if dec_pt_chr==',':
-           csv_delimiter=';'
-       else:
-           csv_delimiter=','
+        locale.setlocale(locale.LC_ALL, '')
+        dec_pt_chr = locale.localeconv()['decimal_point']
+        if dec_pt_chr == ',':
+            csv_delimiter = ';'
+        else:
+            csv_delimiter = ','
     except:
-        logger.warning("Could not grab locale details in order to set the CSV delimiter. Using ',' as the default delimiter.")
-        csv_delimiter=','
+        logger.warning(
+            "Could not grab locale details in order to set the CSV delimiter. Using ',' as the default delimiter.")
+        csv_delimiter = ','
 
     file_to_write = "./tmp/siaas_report_" + \
         datetime.now().strftime('%Y%m%d%H%M%S')+".csv"
