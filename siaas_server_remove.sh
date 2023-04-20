d353da5ac22ad0cb8a9533d004b8a63d921532f1
João Pedro Seara <joao.pedro.seara@gmail.com>
2023-04-20 18:02:08 +0100

    Add index removal from removal script

--- siaas_server_remove.sh
+++ siaas_server_remove.sh
@@ -44,6 +44,7 @@ rm -f /etc/systemd/system/siaas-server.service
 systemctl daemon-reload
 
 echo -e "\nSIAAS Server service and configurations were removed from the system.\n\nThe MongoDB SIAAS DB was kept intact. To remove it manually: \n\n \
+ sudo ${mongo_shell} siaas --eval 'db.siaas.dropIndexes()'\n \
  sudo ${mongo_shell} siaas --eval 'db.dropAllUsers()'\n \
  sudo ${mongo_shell} siaas --eval 'db.dropAllRoles()'\n \
  sudo ${mongo_shell} siaas --eval 'db.dropDatabase()'\n"
